d5d664668a8c94c60e0cb538f35f47729d5431b4
Facundo De Nardo <facundodenardo@hotmail.com>
2020-03-31 11:10:33 +0200

    Copyright terminado

---
+++ copyrightDeFacundoDeNardo/src/main/java/Copyright.java
@@ -0,0 +1,11 @@
+public class Copyright {
+    public static String copyrights(){
+        return "Copyright (C) 2020 Facundo De Nardo - All Rights Reserved\n" +
+                "        You may use, distribute and modify this code under the\n" +
+                "        terms of the XYZ license, which unfortunately won't be\n" +
+                "        written for another century.\n" +
+                "        \n" +
+                "        You should have received a copy of the XYZ license with\n" +
+                "        this file. If not, please visit : http://www.facundo-deNardo.com";
+    }
+}
